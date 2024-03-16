df9d01625348878b021977d7c5a4fc7bad454a53
AbdullahKorayem <abdullah0562007@gmail.com>
2024-03-17 00:54:36 +0200

    editing styling Drawer

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -20,14 +20,14 @@ const NavBar = () => {
             <img
               src="public/amazon-icon/AmzonLogo.png"
               srcSet="../amazon-icon/AmzonLogo.png"
-              className="h-8 w-auto"
+              className="w-auto h-8"
               alt="Amazon Logo"
             />
           </Link>
         </div>
 
         {/* Deliver To */}
-        <div className="border border-transparent px-4 hover:border-white hidden md:block">
+        <div className="hidden px-4 border border-transparent hover:border-white md:block">
           <p className="text-sm">Deliver To</p>
           <div className="flex items-center gap-1">
             <FaLocationDot />
@@ -36,7 +36,7 @@ const NavBar = () => {
         </div>
 
         {/* Search */}
-        <div className="text-black px-4 flex h-10 flex-grow">
+        <div className="flex flex-grow h-10 px-4 text-black">
           <select className="bg-[#E6E6E6] px-2 rounded-lg rounded-r-none ">
             <option>All</option>
           </select>
@@ -45,13 +45,13 @@ const NavBar = () => {
             placeholder="Search Amazon"
             className="w-full pl-2 text-base"
           />
-          <div className="bg-orange-300 flex rounded-lg rounded-l-none items-center p-2">
+          <div className="flex items-center p-2 bg-orange-300 rounded-lg rounded-l-none">
             <FaSearch />
           </div>
         </div>
 
         {/* Language */}
-        <div className="flex items-center border border-transparent p-1 hover:border-white">
+        <div className="flex items-center p-1 border border-transparent hover:border-white">
           <img
             src={
               lang === 'en'
@@ -67,10 +67,10 @@ const NavBar = () => {
             alt="US Flag"
           />
           <select
-            className="bg-transparent font-bold text-base border-none"
+            className="text-base font-bold bg-transparent border-none"
             onChange={e => setLang(e.target.value)}
           >
-            <option className=" bg-slate-900 " value="en">
+            <option className=" bg-slate-900" value="en">
               EN
             </option>
             <option className=" bg-slate-900" value="ar">
@@ -80,17 +80,17 @@ const NavBar = () => {
         </div>
 
         {/* Sign In */}
-        {/* <div className="border border-transparent p-1 hover:border-white">
+        {/* <div className="p-1 border border-transparent hover:border-white">
           <p className="text-sm">Hello, sign in</p>
-          <select className="bg-transparent font-bold text-base py-0 border-none ">
+          <select className="py-0 text-base font-bold bg-transparent border-none ">
             <option className=" bg-slate-900">
               <Link to="/login">Sign in</Link>
             </option>
           </select>
         </div> */}
-        <div className="border border-transparent p-1 hover:border-white mx-2">
+        <div className="p-1 mx-2 border border-transparent hover:border-white">
           <p className="text-sm">Hello, sign in</p>
-          <div className="bg-transparent font-bold text-base py-0 border-none ">
+          <div className="py-0 text-base font-bold bg-transparent border-none ">
             <div className=" bg-slate-900">
               <Link to="/login">Sign in</Link>
             </div>
@@ -98,17 +98,17 @@ const NavBar = () => {
         </div>
 
         {/* Returns & Order */}
-        <div className="border border-transparent p-1 hover:border-white hidden lg:block">
+        <div className="hidden p-1 border border-transparent hover:border-white lg:block">
           <p className="text-xs">Returns</p>
-          <p className="font-bold text-sm">& Order</p>
+          <p className="text-sm font-bold">& Order</p>
         </div>
 
         {/* Cart */}
-        <Badge content="10" className=" text-yellow-400 text-lg ms-5">
+        <Badge content="10" className="text-lg text-yellow-400 ms-5">
           <Link to="/cart">
-            <div className="border border-transparent p-1 hover:border-white flex items-center">
-              <BiCart className="text-5xl mt-2" />
-              <p className="font-bold mt-5">Cart</p>
+            <div className="flex items-center p-1 border border-transparent hover:border-white">
+              <BiCart className="mt-2 text-5xl" />
+              <p className="mt-5 font-bold">Cart</p>
             </div>
           </Link>
         </Badge>
@@ -116,30 +116,30 @@ const NavBar = () => {
 
       {/* Secondary Navigation */}
       <div className="bg-[#222F3D] min-w-full w-[1000px] flex items-center text-white text-sm pl-4">
-        <div className="flex items-center gap-1 border border-transparent p-2 hover:border-white">
-          <FaBars className="font-bold" />
-          <p className=" cursor-pointer" onClick={() => {}}>
-          <CustomDrawer />
+        <div className="flex items-center gap-1 p-2 border border-transparent hover:border-white">
+
+          <p className="cursor-pointer " onClick={() => { }}>
+            <CustomDrawer />
           </p>
         </div>
 
         <ul className="flex items-center">
-          <li className="cursor-pointer gap-1 border border-transparent p-1 hover:border-white">
+          <li className="gap-1 p-1 border border-transparent cursor-pointer hover:border-white">
             <NavLink to="electronics">Electronics</NavLink>
           </li>
-          <li className=" cursor-pointer gap-1 border border-transparent p-1 hover:border-white">
+          <li className="gap-1 p-1 border border-transparent cursor-pointer hover:border-white">
             <NavLink to="kindle">Kindle</NavLink>
           </li>
-          <li className=" cursor-pointer gap-1 border border-transparent p-1 hover:border-white">
+          <li className="gap-1 p-1 border border-transparent cursor-pointer hover:border-white">
             <NavLink to="office-supplies">Office Supplies</NavLink>
           </li>
-          <li className=" cursor-pointer gap-1 border border-transparent p-1 hover:border-white">
+          <li className="gap-1 p-1 border border-transparent cursor-pointer hover:border-white">
             <NavLink to="personal-care">Health & Personal Care</NavLink>
           </li>
-          <li className=" cursor-pointer gap-1 border border-transparent p-1 hover:border-white hidden lg:block">
+          <li className="hidden gap-1 p-1 border border-transparent cursor-pointer hover:border-white lg:block">
             <NavLink to="coffee">Coffee</NavLink>
           </li>
-          <li className=" cursor-pointer gap-1 border border-transparent p-1 hover:border-white hidden lg:block">
+          <li className="hidden gap-1 p-1 border border-transparent cursor-pointer hover:border-white lg:block">
             <NavLink to="watches">Watches</NavLink>
           </li>
         </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
- import { CustomProvider } from 'rsuite';
+import { CustomProvider } from 'rsuite';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <CustomProvider theme="dark">
-       <App />
-    </CustomProvider>
-  </React.StrictMode>,
+      <CustomProvider theme="light">
+         <App />
+      </CustomProvider>
+   </React.StrictMode>,
 )
