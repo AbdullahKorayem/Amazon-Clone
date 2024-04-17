047bf85bfb4952959f9ef69dfe94aced9fd6e0bb
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-04-17 19:34:34 +0200

    add transparent Placeholder

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import NavBar from '../../components/header/NavBar';
 import Footer from '../../components/Footer/Footer';
+import TransparentPlaceholder from '../../components/Placeholder/transparentPlaceholder';
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
   return (
     <>
+      {isLoading && <TransparentPlaceholder />}
       <NavBar />
       <Outlet />
       <Footer />
