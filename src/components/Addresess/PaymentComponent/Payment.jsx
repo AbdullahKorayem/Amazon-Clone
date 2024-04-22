cf0c0b8445e1d6cff98d1a25ca607aab65f72014
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-04-22 13:48:02 +0200

    sloveing

--- src/components/Addresess/PaymentComponent/Payment.jsx
+++ src/components/Addresess/PaymentComponent/Payment.jsx
@@ -15,6 +15,7 @@ export default function Payment({ setInfo, info }) {
           <h1 className="text-xl font-medium text-gray-900 dark:text-white mb-5">
             <input
               type="radio"
+              id="cash"
               value="Cash On Delivery"
               name="card"
               className="mx-2 outline-none"
@@ -23,23 +24,30 @@ export default function Payment({ setInfo, info }) {
               }}
             />
             {t('COD')}
-            <div className="text-sm ms-10">
-              {t('COD_description')}
-              <span className="text-sky-800">{t('learn_more')}</span>
-            </div>
+            <label htmlFor="cash">
+              <div className="text-sm ms-10">
+                {t('COD_description')}
+                <span className="text-sky-800">{t('learn_more')}</span>
+              </div>
+            </label>
           </h1>
           <h1 className="text-xl font-medium text-gray-900 dark:text-white mb-5">
             <input
               type="radio"
               value="Payment Card"
               name="card"
+              id="card"
               className="mx-2 outline-none"
               onClick={() => {
                 setInfo({ ...info, paymentMethod: 'card' });
               }}
             />
-            {t('payment_card')}
-            <div className="text-sm ms-10">{t('payment_card_description')}</div>
+            <label htmlFor="card">
+              {t('payment_card')}
+              <div className="text-sm ms-10">
+                {t('payment_card_description')}
+              </div>
+            </label>
           </h1>
           <hr className="mb-5" />
           {/* <div className="ms-3"><PaymentCards /></div> */}
